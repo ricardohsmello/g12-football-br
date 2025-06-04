5938784582419fb5776b7775a80f652e98c957b6
Ricardo <ricardohsmello@gmail.com>
2025-06-04 13:03:09 -0300

    Creating updateMatchScoreUseCase and its tests

---
+++ application/src/main/java/br/com/g12/usecase/UpdateMatchScoreUseCase.java
@@ -0,0 +1,38 @@
+package br.com.g12.usecase;
+
+
+import br.com.g12.model.Match;
+import br.com.g12.model.Score;
+import br.com.g12.port.MatchPort;
+import br.com.g12.request.ScoreRequest;
+import br.com.g12.validators.MatchValidator;
+import br.com.g12.validators.ScoreValidator;
+
+public class UpdateMatchScoreUseCase {
+
+    private final MatchPort matchPort;
+    private final MatchValidator matchValidator;
+    private final ScoreValidator scoreValidator;
+
+    public UpdateMatchScoreUseCase(
+            MatchPort matchPort,
+            MatchValidator matchValidator,
+            ScoreValidator scoreValidator
+    ) {
+        this.matchPort = matchPort;
+        this.matchValidator = matchValidator;
+        this.scoreValidator = scoreValidator;
+    }
+
+    public void execute(String matchId, ScoreRequest score) {
+        Match match = matchPort.find(matchId);
+        Score model = score.toModel();
+
+        matchValidator.validate(match);
+        scoreValidator.validate(model);
+
+        match.setScore(model);
+
+        matchPort.save(match);
+    }
+}
---
+++ application/src/test/java/br/com/g12/usecase/UpdateMatchScoreUseCaseTest.java
@@ -0,0 +1,53 @@
+package br.com.g12.usecase;
+
+import br.com.g12.model.Match;
+import br.com.g12.model.Score;
+import br.com.g12.port.MatchPort;
+import br.com.g12.request.MatchRequest;
+import br.com.g12.request.ScoreRequest;
+import br.com.g12.response.MatchResponse;
+import br.com.g12.validators.MatchValidator;
+import br.com.g12.validators.ScoreValidator;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+public class UpdateMatchScoreUseCaseTest {
+
+    private MatchPort matchPort;
+    private MatchValidator matchValidator;
+    private ScoreValidator scoreValidator;
+    private UpdateMatchScoreUseCase useCase;
+
+    @BeforeEach
+    void setup() {
+        matchPort = mock(MatchPort.class);
+        matchValidator = mock(MatchValidator.class);
+        scoreValidator = mock(ScoreValidator.class);
+        useCase = new UpdateMatchScoreUseCase(matchPort, matchValidator, scoreValidator);
+    }
+
+    @Test
+    public void should_create_match_successfully() {
+        String matchId = "match-123";
+        ScoreRequest request = new ScoreRequest(2, 1);
+        Score score = new Score(2, 1);
+
+        Match match = new Match(matchId, 1, "Real Madrid", "Corinthians", new Date(), null);
+
+        when(matchPort.find(any(String.class))).thenReturn(match);
+        useCase.execute(matchId, request);
+
+        verify(matchPort).find(matchId);
+        verify(matchValidator).validate(match);
+        verify(scoreValidator).validate(score);
+        verify(matchPort).save(match);
+
+        assert match.getScore().equals(score);
+    }
+}
