4c3a7bd4487e4464725c2e4ca9f5359a155ddbca
Ricardo <ricardohsmello@gmail.com>
2025-06-04 13:05:24 -0300

    Creating Match restController

---
+++ infrastructure/src/main/java/br/com/g12/http/MatchController.java
@@ -0,0 +1,34 @@
+package br.com.g12.http;
+
+import br.com.g12.request.MatchRequest;
+import br.com.g12.request.ScoreRequest;
+import br.com.g12.response.MatchResponse;
+import br.com.g12.usecase.CreateMatchUseCase;
+import br.com.g12.usecase.UpdateMatchScoreUseCase;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/match")
+public class MatchController {
+
+    private final CreateMatchUseCase createMatchUseCase;
+    private final UpdateMatchScoreUseCase updateMatchScoreUseCase;
+
+    MatchController(CreateMatchUseCase createMatchUseCase, UpdateMatchScoreUseCase updateMatchScoreUseCase) {
+        this.createMatchUseCase = createMatchUseCase;
+        this.updateMatchScoreUseCase = updateMatchScoreUseCase;
+    }
+
+    @PostMapping
+    public ResponseEntity<MatchResponse> addMatch(@RequestBody MatchRequest matchRequest) {
+        MatchResponse matchResponse = createMatchUseCase.execute(matchRequest);
+        return ResponseEntity.ok(matchResponse);
+    }
+
+    @PutMapping("/{id}/score")
+    public ResponseEntity<Void> updateScore(@PathVariable String id, @RequestBody ScoreRequest scoreRequest) {
+        updateMatchScoreUseCase.execute(id, scoreRequest);
+        return ResponseEntity.noContent().build();
+    }
+}
