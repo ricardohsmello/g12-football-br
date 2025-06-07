9ee05648453c92f8a04b46dba1a1e91faad644cb
Ricardo <ricardohsmello@gmail.com>
2025-06-07 01:51:28 -0300

    Ref

--- backend/infrastructure/src/main/java/br/com/g12/config/ApplicationServiceConfig.java
+++ backend/infrastructure/src/main/java/br/com/g12/config/ApplicationServiceConfig.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ApplicationServiceConfig {
-
     @Bean
     public ScoringService scoringService() {
         return new ScoringService();
