6d049e6c4a1544ad6bacb6f4137145d6b26c9b33
Ricardo <ricardohsmello@gmail.com>
2025-06-10 20:51:40 -0300

    Including match score edit and bet edit

---
+++ frontend/src/app/services/bet-service/bet.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import { Bet } from '../../domain/model/bet/bet' 
+
+@Injectable({
+  providedIn: 'root'
+})export class BetService {
+
+  private betURL: string;
+
+  constructor(private http: HttpClient) {
+    this.betURL = 'http://localhost:8080/bet';
+  }
+
+  public save(bet: Bet) {
+    console.log(this.betURL);
+    return this.http.post<Bet>(this.betURL, bet);
+  }
+}
