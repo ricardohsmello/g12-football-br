2539da653622060a189db7046a2130e0075f71b7
Ricardo <ricardohsmello@gmail.com>
2025-06-13 23:31:24 -0300

    Changing API URL for production environment (should use from file later)

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://g12-football-br-1.onrender.com',
+  apiUrl: 'https://g12-football-br-1-qdun.onrender.com',
   keycloak: {
     url: 'https://g12-football-br-1.onrender.com',
     realm: 'g12',
